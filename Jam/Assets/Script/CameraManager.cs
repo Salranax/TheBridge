c1612523091719dbb5d91f8b6556262fc2caee43
Unknown <alkilicalp@gmail.com>
2020-03-09 16:14:41 +0300

    cam positions added

--- Jam/Assets/Script/CameraManager.cs
+++ Jam/Assets/Script/CameraManager.cs
@@ -13,15 +13,37 @@ public class CameraManager : MonoBehaviour
     public CinemachineVirtualCamera playerCamRIGHT;
     public CinemachineVirtualCamera playerCamBACK;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void activateStartCam(){
+        resetCams();
+        startCam.Priority = 10;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void activatePlayerCam(){
+        resetCams();
+        playerCam.Priority = 10;
+    }
+
+    public void activateLeftCam(){
+        resetCams();
+        playerCamLEFT.Priority = 10;
+    }
+
+    public void activateRightCam(){
+        resetCams();
+        playerCamRIGHT.Priority = 10;
+    }
+
+    public void activateBackCam(){
+        resetCams();
+        playerCamBACK.Priority = 10;
+    }
+
+    public void activateTopCam(){
+        resetCams();
+        playerCamTOP.Priority = 10;
+    }
+
+    private void resetCams(){
+        startCam.Priority = playerCam .Priority = playerCamTOP.Priority = playerCamLEFT.Priority = playerCamRIGHT.Priority = playerCamBACK.Priority = 8;
     }
 }
